cb129cc11a88553280f9a775fda9b5381df6487f
XxDEAMxX <deam10hw@gmail.com>
2023-07-11 21:29:48 -0500

    bottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('HomeScreen');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aatrox'),
+      ),
+      body: Center(
+        child: const Text('HomeScreen')
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+       items: [
+        BottomNavigationBarItem(icon: Icon(Icons.payments_outlined), label: 'Prestamos'),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Clientes'),
+        BottomNavigationBarItem(icon: Icon(Icons.money_off_csred_rounded), label: 'Gastos'),
+        
+       ], 
+      )
+    );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,116 +11,113 @@ class LoginScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Center(
-                  child: _imageLogin(),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric( horizontal: 20),
+          child: Column(
+            children: [
+              const Center(
+                child: _imageLogin(),
+              ),
+              SizedBox(height: 30,),
+              Text('Ingresa con tu Cuenta', style: TextStyle(fontSize: 15),),
+              SizedBox(height: 20,),
+      
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    PhysicalModel(
+                      color: Color.fromRGBO(0, 0, 0, 0.1),
+                      child: TextFormField(
+                        autocorrect: false,
+                        decoration: InputDecorations.authInputDecoration(labelText: 'Correo Electronico'),
+                        
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    PhysicalModel(
+                      color: Color.fromRGBO(0, 0, 0, 0.1),
+                      child: TextFormField(
+                        autocorrect: false,
+                        decoration: InputDecorations.authInputDecoration(labelText: 'Correo Electronico'),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+      
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'home');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      disabledColor: Colors.grey,
+                      elevation: 2,
+                      color: Colors.red,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 90, vertical: 20),
+                        child: Text(
+                          'Iniciar seción'
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                SizedBox(height: 30,),
-                Text('Ingresa con tu Cuenta', style: TextStyle(fontSize: 15),),
-                SizedBox(height: 20,),
-        
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      PhysicalModel(
-                        color: Color.fromRGBO(0, 0, 0, 0.1),
-                        child: TextFormField(
-                          autocorrect: false,
-                          decoration: InputDecorations.authInputDecoration(labelText: 'Correo Electronico'),
+              ),
+              SizedBox(height: 60,),
+      
+              Container(
+                child: Column(
+                  children: [
+                    Center(child: Text('Tambien puedes ingresa con', style: TextStyle(fontSize: 15))),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MaterialButton(
+                          padding: EdgeInsets.symmetric(vertical: 10),
                           
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-                      PhysicalModel(
-                        color: Color.fromRGBO(0, 0, 0, 0.1),
-                        child: TextFormField(
-                          autocorrect: false,
-                          decoration: InputDecorations.authInputDecoration(labelText: 'Correo Electronico'),
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-        
-                      MaterialButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'home');
-                        },
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                        disabledColor: Colors.grey,
-                        elevation: 2,
-                        color: Colors.red,
-                        child: Container(
-                          padding: EdgeInsets.symmetric(horizontal: 90, vertical: 20),
-                          child: Text(
-                            'Iniciar seción'
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(height: 60,),
-        
-                Container(
-                  child: Column(
-                    children: [
-                      Center(child: Text('Ingresa tambien con', style: TextStyle(fontSize: 15))),
-                      SizedBox(height: 20,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          MaterialButton(
-                            padding: EdgeInsets.symmetric(vertical: 10),
+                          elevation: 1,
+                          color: Colors.white,
+                          onPressed: () {
                             
-                            elevation: 1,
-                            color: Colors.white,
-                            onPressed: () {
-                              
-                            },
-                            child: Container(
-                              
-                              child: Image(image: AssetImage('assets/google.png'),),
-                            ),
+                          },
+                          child: Container(
+                            
+                            child: Image(image: AssetImage('assets/google.png'), color: const Color.fromARGB(255, 136, 11, 11)),
                           ),
-                          MaterialButton(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            elevation: 1,
-                            color: Colors.white,
-                            onPressed: () {
-                              
-                            },
-                            child: Container(
-                              child: Image(image: AssetImage('assets/facebook.png')),
-                            ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          elevation: 1,
+                          color: Colors.white,
+                          onPressed: () {
+                            
+                          },
+                          child: Container(
+                            child: Image(image: AssetImage('assets/facebook.png'), color: const Color.fromARGB(255, 136, 11, 11)),
                           ),
-                          MaterialButton(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            elevation: 1,
-                            color: Colors.white,
-                            onPressed: () {
-                              
-                            },
-                            child: Container(
-                              child: Image(image: AssetImage('assets/twitter.png')),
-                            ),
+                        ),
+                        MaterialButton(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          elevation: 1,
+                          color: Colors.white,
+                          onPressed: () {
+                            
+                          },
+                          child: Container(
+                            child: Image(image: AssetImage('assets/twitter.png'), color: const Color.fromARGB(255, 136, 11, 11)),
                           ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
-                
-                
-              ],
-            ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+              
+              
+            ],
           ),
         ),
       )
